86abe6f26dbd225b0df464e585f8d90a5cc4c29d
AishwaryaSriyapuReddy <aishwarya.sriyapureddy0@gmail.com>
2025-06-07 15:35:44 -0400

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import  train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','data.csv')
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):  #write code to read from database or files or from api.
+        logging.info('Entered the data ingestion method or component')
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')   # reading data from file. here you can read data from mongodb client or SQL client
+            logging.info('Read the dataset as a dataframe')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #check directory(artifacts) of train_path exists, if it doesn't exists create one
+            
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) #saved imported df in raw_data_path
+            
+            logging.info('Train test split initiated')
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42) #Split raw data into train and test
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) # saving data to the path
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False, header=True)
+            
+            logging.info('Ingestion of the data is completed')  
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
